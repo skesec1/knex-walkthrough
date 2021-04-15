9030b412a38dc930634d2f9c2cd08b1e2812f3e9
Sam <69992785+skesec1@users.noreply.github.com>
2021-04-15 14:12:39 -0400

    Add files via upload

---
+++ knexfile.js
@@ -0,0 +1,56 @@
+// module.exports = require('knex')({
+//   client: 'pg',
+//   connection: {
+//     host: '127.0.0.1',
+//     port: '5432',
+//     user: 'postgres',
+//     password: 'docker',
+//     database: 'vacations'
+//   }
+// });
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    // connection: {
+    //   user: 'postgres',
+    //   password: 'docker',
+    //   database: 'movies'
+      connection: 'postgres://postgres:docker@localhost/movies'
+    }
+  // },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
